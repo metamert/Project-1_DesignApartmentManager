7c44f542a21899b12b60f190bbd36e02d962c84d
mertyjsx <orkunmertgs41@gmail.com>
2021-01-05 17:18:10 +0300

    upload server

---
+++ SQL-server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+//middleware
+
+app.use(cors());
+app.use(express.json());
+
+//routes
+
+app.use("/authentication", require("./routes/jwtAuth"));
+app.use("/admin", require("./routes/admin"));
+app.use("/dues", require("./routes/due"));
+
+app.use("/dashboard", require("./routes/dashboard"));
+
+app.listen(5000, () => {
+  console.log(`Server is starting on port 5000`);
+});
---
+++ SQL-server/utils/jwtGenerator.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+function jwtGenerator(payload) {
+  
+
+  //the code below was the code written from the tutorial
+  //Look at file server/routes/dashboard.js to see the change code for this code
+
+  //   function jwtGenerator(user_id) {
+  //   const payload = {
+  //     user: user_id
+  //   };
+
+  return jwt.sign(payload, process.env.jwtSecret);
+}
+
+module.exports = jwtGenerator;
